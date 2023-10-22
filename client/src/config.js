eaa24364f7f0b767e470991c8ecfe8f85c941d87
CogitoErgoSum746 <jamesgracious123@gmail.com>
2023-10-22 15:34:02 +0530

    updated

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,2 @@
 export const API_BASE_URL = 'http://localhost:8001/api'
 // export const API_BASE_URL = 'https://mindmatrix.successteps.in/api'
-// export const API_BASE_URL = 'https://34.93.210.152/api'
