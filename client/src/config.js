97e57ba90c1c9dbe58e1edfd1a6d78603a98a859
root <root@server-103-212-120-75.da.direct>
2023-11-03 11:00:31 -0700

    ok

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,3 @@
 // export const API_BASE_URL = 'http://localhost:8001/api'
-// export const API_BASE_URL = 'https://mindmatrix.successteps.in/api'
 // export const API_BASE_URL = 'https://103.212.120.75/api'
-// export const API_BASE_URL = 'https://mindmatrix.successteps.in/api'
 export const API_BASE_URL = 'https://successteps.in/api'
