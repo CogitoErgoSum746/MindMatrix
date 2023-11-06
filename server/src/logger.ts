f4385bf78860f3bc098a23e7438c1611241adaa4
CogitoErgoSum746 <jamesgracious123@gmail.com>
2023-11-06 18:33:23 +0530

    updated logger for IST

--- server/src/logger.ts
+++ server/src/logger.ts
@@ -1,21 +1,27 @@
 import { createLogger, transports, format } from 'winston';
 import 'winston-daily-rotate-file';
+import moment from 'moment';
 
-const { printf, combine, timestamp } = format;
+const { printf, combine } = format;
 const { DailyRotateFile } = transports;
 
-const errorLogFormat = printf(({ timestamp, level, message }) => {
-  return `${timestamp} [${level}]: ${message}`;
+const customTimestamp = () => {
+  // Use moment.js to format the timestamp with date and time in IST
+  return moment().utcOffset('+05:30').format('YYYY-MM-DD HH:mm:ss');
+};
+
+const errorLogFormat = printf(({ level, message }) => {
+  return `${customTimestamp()} [${level}]: ${message}`;
 });
 
-const appLogFormat = printf(({ timestamp, level, message }) => {
-  return `${timestamp} [${level}]: ${message}`;
+const appLogFormat = printf(({ level, message }) => {
+  return `${customTimestamp()} [${level}]: ${message}`;
 });
 
 const createLoggerWithOptions = (dirname: string, level: string, filename: string) => {
   return createLogger({
     level,
-    format: combine(timestamp(), level === 'error' ? errorLogFormat : appLogFormat),
+    format: combine(level === 'error' ? errorLogFormat : appLogFormat),
     transports: [
       new DailyRotateFile({
         dirname,
