dcbd9260f0c7dfcc3aad65d68ddc1cabb535db63
CogitoErgoSum746 <jamesgracious123@gmail.com>
2023-12-03 19:51:43 +0530

    created sanity project successteps blog

--- client/src/config.js
+++
@@ -1,2 +0,0 @@
-export const API_BASE_URL = 'http://localhost:8001/api'
-// export const API_BASE_URL = 'https://mindmatrix.successteps.in/api'
---
+++ successteps-blog/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'ydie9qvv',
+    dataset: 'production'
+  }
+})
