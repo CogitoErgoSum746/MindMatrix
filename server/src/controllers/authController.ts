c4a8fe09f381d5c19ebe283d4b4356dcacc0f2f7
CogitoErgoSum746 <jamesgracious123@gmail.com>
2023-12-03 16:50:39 +0530

    auth fixin

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -127,6 +127,7 @@ export async function login(req: Request, res: Response): Promise<any> {
   }
   try {
     const { username, password } = req.body;
+    console.log(username);
 
     const AdminEmail = process.env.ADMIN_EMAIL;
 
