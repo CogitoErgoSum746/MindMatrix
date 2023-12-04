56a6e26e8b1b4d3b0b11fbc502c94b64636eac6c
CogitoErgoSum746 <jamesgracious123@gmail.com>
2023-12-04 19:33:39 +0530

    fixing blog post page 2

--- client/src/Pages/Blog/SinglePost.js
+++ client/src/Pages/Blog/SinglePost.js
@@ -3,6 +3,7 @@ import { Link, useParams } from "react-router-dom";
 import { sanityClient } from "../../client";
 import { format } from "date-fns";
 import { PortableText } from "@portabletext/react";
+import Navbar from "../../components/Navbar";
 // import Profile from "../components/Profile";
 
 export default function SinglePost() {
@@ -39,16 +40,17 @@ export default function SinglePost() {
 
   return (
     <>
+    <Navbar />
       {blogpost && (
-        <section className="py-20 px-5 max-w-3xl mx-auto">
+        <section className="bg-gray-100 py-16 px-4 md:px-8 lg:px-12 xl:px-24 max-w-6xl mx-auto text-left">
           {blogpost.mainImage && (
             <img
               src={blogpost.mainImage.asset.url}
               alt={blogpost.mainImage.alt}
-              className="h-2/3 w-full object-cover rounded-2xl shadow"
+              className="h-auto w-full object-cover rounded-lg shadow"
             />
           )}
-          <h1 className="text-4xl my-8 xl:text-6xl">{blogpost.title}</h1>
+          <h1 className="text-4xl my-8 xl:text-4xl font-bold font-">{blogpost.title}</h1>
           <p className="font-bold text-sm mb-8">
             By {blogpost.name}{" "}
             {blogpost.publishedAt && (
@@ -58,13 +60,22 @@ export default function SinglePost() {
               </>
             )}
           </p>
-
+          <div className="text-left text-lg leading-relaxed">
           <PortableText value={blogpost.body} />
-
+          </div>
+          
           <div className="max-w-7xl mx-auto px-5 mb-20 mt-10 flex items-end justify-end">
+          <Link
+              to="/facilitator"
+              // className="bg-white dark:bg-slate-800 dark:hover:bg-slate-700 py-2 px-8 rounded shadow text-slate-800 dark:text-slate-400 tracking-wide hover:opacity-75 transition-all duration-200"
+              className="p-2 mx-2 text-center rounded-full bg-gradient-to-r from-orange-500 to-yellow-500 text-black font-bold hover:from-yellow-600 hover:to-orange-600 focus:outline-none w-full md:w-48 transition duration-300 ease-in-out transform hover:scale-105"
+            >
+              Author's Profile
+            </Link>
             <Link
               to="/successteps-blog"
-              className="bg-white dark:bg-slate-800 dark:hover:bg-slate-700 py-2 px-8 rounded shadow text-slate-800 dark:text-slate-400 tracking-wide hover:opacity-75 transition-all duration-200"
+              // className="bg-white dark:bg-slate-800 dark:hover:bg-slate-700 py-2 px-8 rounded shadow text-slate-800 dark:text-slate-400 tracking-wide hover:opacity-75 transition-all duration-200"
+              className="p-2 text-center rounded-full bg-gradient-to-r from-orange-500 to-yellow-500 text-black font-bold hover:from-yellow-600 hover:to-orange-600 focus:outline-none w-full md:w-48 transition duration-300 ease-in-out transform hover:scale-105"
             >
               Read More Blog posts
             </Link>
