cf6071b85084e5302be33dce36e55ccc87976f92
CogitoErgoSum746 <jamesgracious123@gmail.com>
2023-12-04 08:57:26 +0530

    initialized sanity/client

--- client/src/AllRoutes.js
+++ client/src/AllRoutes.js
@@ -31,6 +31,8 @@ import PsychometricTestInfo from "./Pages/PsychometricTest/PsychometricTestInfo"
 import FloatingIcon from "./components/FloatingIcon";
 import ContactUs from "./Pages/Contactus";
 import Termsconds from "./Pages/About/Terms&Conditions";
+import allPosts from "./Pages/Blog/allPosts";
+import SinglePost from "./Pages/Blog/SinglePost";
 import Footer from "./components/HomePage/Footer";
 import PageNotFound from "./Pages/PageNotFound";
 
@@ -91,6 +93,9 @@ function AllRoutes() {
         <Route path="/ContactUs" element={<ContactUs />} />
         <Route path="/psychometrictest" element={<PsychometricTestInfo />} />
         <Route path="/psychometrictest/getstarted" element={<Home1 />} />
+
+        <Route path="/successteps-blog" element={<allPosts />} />
+        <Route path="/successteps-blog/:slug" element={<SinglePost />} />
 
         <Route path="/admin" element={authtoken ? <AdminPanel /> : <Navigate to='/login' />} />
         <Route
